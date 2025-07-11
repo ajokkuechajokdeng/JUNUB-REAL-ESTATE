fdf392fca0dfd9275691f61afafd326f654f3ee1
ajokkuechajokdeng <a.deng2@alustudent.com>
2025-07-11 02:05:22 +0200

    update

--- build.sh
+++ build.sh
@@ -1,10 +1,14 @@
 #!/usr/bin/env bash
+set -o errexit  # Exit on error
 
-set -o errexit  # exit on error
-
-cd Backend
-
+# Install dependencies from project root
 pip install -r requirements.txt
 
+# Move into Backend folder where manage.py is
+cd Backend
+
+# Collect static files
 python manage.py collectstatic --no-input
+
+# Apply migrations
 python manage.py migrate
